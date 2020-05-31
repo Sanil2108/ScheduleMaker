c2c5d2db8035cd9e2a8bb69e0f65b3ca9b38f91b
Sanil Khurana <sanilkhurana7@gmail.com>
2020-05-31 07:00:58 +0530

    Updated location of --noreload

--- start.sh
+++ start.sh
@@ -14,5 +14,5 @@ sudo chown -R ubuntu $root/venv
 source $root/venv/bin/activate
 
 pip3 install -r $root/backend/requirements.txt
-python3 $root/backend/manage.py --noreload runserver &
+python3 $root/backend/manage.py runserver --noreload &
 lsof -i :8000
