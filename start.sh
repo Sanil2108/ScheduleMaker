c1d8ae79d9dfe0f0c1305f152ceda80f6cc6a5bc
Sanil Khurana <sanilkhurana7@gmail.com>
2020-05-31 06:39:14 +0530

    Added permissions

--- start.sh
+++ start.sh
@@ -14,4 +14,4 @@ sudo chown -R ubuntu $root/venv
 source $root/venv/bin/activate
 
 pip3 install -r $root/backend/requirements.txt
-python3 $root/backend/manage.py runserver
+python3 $root/backend/manage.py --no-reload runserver &
