da3c7278254b8edc66ebf346d3123021a13b1c4f
Sanil Khurana <sanilkhurana7@gmail.com>
2020-05-31 06:17:03 +0530

    Added $root

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 ls -lat
-sudo unzip -o backend.zip
+root=/home/ubuntu/schedulemaker
+
+sudo unzip -o $root/backend.zip
 
 sudo apt-get install -y python3 python3-pip
 sudo apt-get install -y virtualenv
 sudo apt-get install -y mysql-server
 sudo apt-get install -y libmysqlclient-dev
 
-sudo virtualenv -p python3 venv
-sudo chown -R ubuntu venv
-source venv/bin/activate
+sudo virtualenv -p python3 $root/venv
+sudo chown -R ubuntu $root/venv
+source $root/venv/bin/activate
 
-pip3 install -r backend/requirements.txt
-python3 backend/manage.py runserver
+pip3 install -r $root/backend/requirements.txt
+python3 $root/backend/manage.py runserver
