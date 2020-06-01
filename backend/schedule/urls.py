07de84e48e2ecb11e9c3848ae3709fa291d27636
Sanil Khurana <sanilkhurana7@gmail.com>
2020-06-02 01:25:22 +0530

    BugFix: Can now fetch schedules properly

--- backend/schedule/urls.py
+++ backend/schedule/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', ScheduleView.as_view(), name = 'Schedule detail'),
     re_path(r'^(?P<schedule_id>[\d]+)/$', ScheduleView.as_view(), name = 'Schedule detail with parameter'),
     path('list/', ScheduleListView.as_view(), name = 'All schedules'),
-    path('dates_list/', UserScheduleDateListView.as_view(), name = 'All schedules for user based on dates'),
+    re_path(r'^dates_list[/?]$', UserScheduleDateListView.as_view(), name = 'All schedules for user based on dates'),
     re_path(r'^(?P<schedule_id>[\d]+)/events/(?P<event_id>[\d]*)$', EventView.as_view(), name = 'Events detail'),
     path('<int:schedule_id>/comments/', CommentView.as_view(), name = 'Comments detail'),
 ]
